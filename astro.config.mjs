56c6b929b0c994206f6b036c9d13e338984e2223
iamolegga <iamolegga@gmail.com>
2026-08-12 19:40:59 +0300

    feat: restyling

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,88 @@ export default defineConfig({
 				replacesTitle: true,
 			},
 			favicon: '/favicon.svg',
+			customCss: ['./src/styles/docs.css'],
+			components: {
+				Header: './src/components/docs/Header.astro',
+				ThemeProvider: './src/components/docs/ThemeProvider.astro',
+				ThemeSelect: './src/components/docs/ThemeSelect.astro',
+			},
+			head: [
+				{
+					tag: 'link',
+					attrs: { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossorigin: true,
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'stylesheet',
+						href: 'https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&family=JetBrains+Mono:wght@400;500;700&family=Martian+Mono:wght@400;500&display=swap',
+					},
+				},
+			],
+			expressiveCode: {
+				themes: [
+					{
+						name: 'monolock',
+						type: 'dark',
+						colors: {
+							'editor.background': '#191715',
+							'editor.foreground': '#d8d3cc',
+						},
+						tokenColors: [
+							{
+								scope: ['comment', 'punctuation.definition.comment'],
+								settings: { foreground: '#5c564f' },
+							},
+							{
+								scope: ['string', 'punctuation.definition.string'],
+								settings: { foreground: '#8fb3a8' },
+							},
+							{
+								scope: [
+									'constant.numeric',
+									'constant.language',
+									'constant.other',
+								],
+								settings: { foreground: '#e5c07b' },
+							},
+							{
+								scope: ['keyword', 'storage.type', 'storage.modifier'],
+								settings: { foreground: '#6d6660' },
+							},
+							{
+								scope: ['entity.name.function', 'support.function'],
+								settings: { foreground: '#f4f1ec' },
+							},
+							{
+								scope: ['variable', 'entity.name'],
+								settings: { foreground: '#d8d3cc' },
+							},
+						],
+					},
+				],
+				styleOverrides: {
+					borderColor: '#2e2b28',
+					borderRadius: '10px',
+					codeFontSize: '0.8125rem',
+					frames: {
+						editorTabBarBackground: '#151412',
+						editorActiveTabBackground: '#191715',
+						editorActiveTabIndicatorTopColor: '#f0b429',
+						terminalTitlebarBackground: '#151412',
+						terminalTitlebarBorderBottomColor: '#211f1d',
+						terminalBackground: '#191715',
+					},
+				},
+			},
 			social: [
 				{
 					icon: 'github',
